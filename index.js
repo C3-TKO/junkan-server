0ff04a395b45cb247b84555b009b2bb06828f6ef
Thomas Kolar <thomas.kolar@email.de>
2017-07-14 17:48:46 +0200

    Adds basic implementation of the junkan server

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-/**
- * Created by tomtom on 14.07.17.
- */
+"use strict"
+
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Junkan server is up and running');
+});
+
+app.get('/title/:url', function (req, res) {
+    var url = decodeURI(req.params.url);
+
+    let result = {
+        "title": ""
+    };
+
+    request(url, function(error, response, html) {
+        // console.log(response);
+        if(!error){
+            var $ = cheerio.load(html);
+            result.title = $('title').text();
+        }
+
+        res.send(result);
+    });
+});
+
+app.get('/html/:url', function (req, res) {
+    var url = decodeURI(req.params.url);
+
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
