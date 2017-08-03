a44bf1f29495957b40230aabe919254a209854d6
Thomas Kolar <thomas.kolar@email.de>
2017-08-03 23:31:51 +0200

    feat: Add final route catching for handling 404 on api

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 "use strict"
 
 const express = require('express');
+const HTTPStatus = require('http-status-codes');
 
 var scraper = require('./app/routes/scraper');
 
 const app = express();
 
 app.use('/', scraper);
+
+// Final catch any route middleware used to raise 404
+app.get('*', (req, res, next) => {
+  const err = new Error();
+  err.status = HTTPStatus.NOT_FOUND;
+  next(err);
+});
+
+// Error response handler
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.send(err.message || '** no unicorns here **');
+});
 
 app.listen(3000, function () {
     console.log('Junkan server is running on port 3000!');
