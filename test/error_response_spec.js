d248d8946c9080079b2561cfa6a6a3484b5a328e
Thomas Kolar <thomas.kolar@email.de>
2017-08-05 11:26:39 +0200

    refactor: Remove duplicated code

--- test/error_response_spec.js
+++ test/error_response_spec.js
@@ -31,22 +31,14 @@ describe('Invalid routes', () => {
 
 
 describe('Invalid input parameters', () => {
+
+
   describe('/GET/title/:invalid_url', () => {
     it('should respond with a 400 error object', (done) => {
       chai.request(server)
         .get('/title/htINVALIDtps%3A%2F%2Fwww.google.com')
         .end((err, res) => {
-          res.should.have.status(HTTPStatus.BAD_REQUEST);
-          res.headers['content-type'].should.equal('application/json; charset=utf-8');
-          res.headers['content-language'].should.equal('en');
-          res.body.should.eql(
-						{
-							type: "about:blank",
-								title: "Bad Request",
-							status: HTTPStatus.BAD_REQUEST,
-							detail: "Parameter url is invalid"
-						}
-					);
+          assertErrorResponseSpecification(res);
           done();
         });
     });
@@ -55,21 +47,25 @@ describe('Invalid input parameters', () => {
   describe('/GET/html/:invalid_url', () => {
     it('should respond with a 400 error object', (done) => {
       chai.request(server)
-        .get('/title/htINVALIDtps%3A%2F%2Fwww.google.com')
+        .get('/html/htINVALIDtps%3A%2F%2Fwww.google.com')
         .end((err, res) => {
-          res.should.have.status(HTTPStatus.BAD_REQUEST);
-          res.headers['content-type'].should.equal('application/json; charset=utf-8');
-          res.headers['content-language'].should.equal('en');
-          res.body.should.eql(
-            {
-              type: "about:blank",
-              title: "Bad Request",
-              status: HTTPStatus.BAD_REQUEST,
-              detail: "Parameter url is invalid"
-            }
-          );
+          assertErrorResponseSpecification(res);
           done();
         });
     });
   });
 });
+
+assertErrorResponseSpecification = (res) => {
+  res.should.have.status(HTTPStatus.BAD_REQUEST);
+  res.headers['content-type'].should.equal('application/json; charset=utf-8');
+  res.headers['content-language'].should.equal('en');
+  res.body.should.eql(
+    {
+      type: "about:blank",
+      title: "Bad Request",
+      status: HTTPStatus.BAD_REQUEST,
+      detail: "Parameter url is invalid"
+    }
+  );
+}
