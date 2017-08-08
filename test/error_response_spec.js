48fd4bad0ac49f8ba86941c5adfc5c00d2e4a752
Thomas Kolar <thomas.kolar@email.de>
2017-08-08 20:59:26 +0200

    test: Add spec for error response handling

--- test/error_response_spec.js
+++ test/error_response_spec.js
@@ -2,7 +2,9 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index');
 const should = chai.should();
+const sinon = require('sinon');
 const HTTPStatus = require('http-status-codes');
+const handleErrorResponse = require('../app/errorResponse');
 
 chai.use(chaiHttp);
 
@@ -75,6 +77,155 @@ describe('Invalid input parameters', () => {
     });
   });
 
+});
+
+
+describe('Error response handling', () => {
+
+  //handleErrorResponse(err, req, res, next)
+  const res = {};
+  res.send = sinon.spy();
+  res.status = sinon.spy();
+
+  beforeEach(() => {
+    res.send.reset();
+    res.status.reset();
+  });
+
+  it('should handle a defined HTTP Code', (done) => {
+    const err =
+      {
+        statusCode: HTTPStatus.BAD_REQUEST,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.status.withArgs(HTTPStatus.BAD_REQUEST).calledOnce.should.equal(true);
+    res.send.calledOnce;
+    done();
+  });
+
+  it('should handle an undefined HTTP CODE', (done) => {
+    const err =
+      {
+        statusCode: undefined,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.status.withArgs(HTTPStatus.INTERNAL_SERVER_ERROR).calledOnce.should.equal(true);
+    res.send.calledOnce;
+    done();
+  });
+
+  it('should handle error type', (done) => {
+    const err =
+      {
+        type: 'TEST',
+      };
+
+    const errorResponse =
+      {
+        type: 'TEST',
+        title: HTTPStatus.getStatusText(HTTPStatus.INTERNAL_SERVER_ERROR),
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
+
+  it('should handle undefined error type', (done) => {
+    const err = {};
+
+    const errorResponse =
+      {
+        type: 'about:blank',
+        title: HTTPStatus.getStatusText(HTTPStatus.INTERNAL_SERVER_ERROR),
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
+
+  it('should handle error messages', (done) => {
+    const err =
+      {
+        message: 'TEST',
+      };
+
+    const errorResponse =
+      {
+        type: 'about:blank',
+        title: 'TEST',
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
+
+  it('should handle undefined error messages', (done) => {
+    const err =
+      {
+        message: undefined,
+      };
+
+    const errorResponse =
+      {
+        type: 'about:blank',
+        title: HTTPStatus.getStatusText(HTTPStatus.INTERNAL_SERVER_ERROR),
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
+
+  it('should handle error detail', (done) => {
+    const err =
+      {
+        detail: 'TEST',
+      };
+
+    const errorResponse =
+      {
+        type: 'about:blank',
+        title: HTTPStatus.getStatusText(HTTPStatus.INTERNAL_SERVER_ERROR),
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+        detail: 'TEST',
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
+
+  it('should handle undefined error detail', (done) => {
+    const err = {};
+
+    const errorResponse =
+      {
+        type: 'about:blank',
+        title: HTTPStatus.getStatusText(HTTPStatus.INTERNAL_SERVER_ERROR),
+        status: HTTPStatus.INTERNAL_SERVER_ERROR,
+      };
+
+    handleErrorResponse(err, null, res, null);
+
+    res.send.withArgs(errorResponse).calledOnce.should.equal(true);
+    done();
+  });
 });
 
 assertErrorResponseSpecificationInvalidURLSyntax = (res) => {
